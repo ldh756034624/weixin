493d094695d5f723421aecaa5ba844c3ddb0c65d
luosisi2020 <478066577@qq.com>
2017-11-11 14:55:15 +0800

    index

--- src/views/index.vue
+++ src/views/index.vue
@@ -70,9 +70,11 @@ export default {
     let self=this;
     self.setTitle('欢乐之家');
     self.WxCode = self.$route.query.code;
-    self.getWxCode()
+    
     if(self.WxCode){
       self.weChatLogin();
+    }else{
+      self.getWxCode()
     }
     
   },
@@ -82,7 +84,6 @@ export default {
       var redirectUrl=Vue.http.defaults.baseURL+'/h9-weixin/#/index'
       console.log(redirectUrl)
       console.log("base64=="+encode(redirectUrl))
-      //window.location.href='/sh/ja/v1/wechat/auth?autoLogin=true&url='+encode(wxRedirectUrl)
       window.location.href=Vue.http.defaults.baseURL+'/h9/api/common/wechat/code?url='+encode(redirectUrl)
     },
     weChatLogin:function(){
