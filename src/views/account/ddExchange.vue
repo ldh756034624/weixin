4b0fb192ffb7f30b2b0e1346b3fe4a4d5008400f
wudong <wudong@hey900.com>
2017-12-25 14:35:02 +0800

    兑换卡券时，错误提示

--- src/views/account/ddExchange.vue
+++ src/views/account/ddExchange.vue
@@ -72,7 +72,7 @@ export default {
         if (res.data.code === 0) {
           this.codeAlert=true;
         } else {
-          _g.toastMsg('error', res.msg)
+          _g.toastMsg('error', res.data.msg)
         }
       })
     },
