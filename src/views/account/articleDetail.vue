c4d64bb43e76496593cb201533c548f8d25a1e2c
wudong <wudong@hey900.com>
2018-01-16 10:41:45 +0800

    酒店详情

--- src/views/account/articleDetail.vue
+++ src/views/account/articleDetail.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="articlePage">
+  <div class="articlePage">
     <p class="title">{{articleData.title}}</p>
     <p class="date"><span>{{articleData.createTime}}</span>{{articleData.userName}}</p>
     <div class="content" v-if="isHtml" v-html='articleData.content'></div>
@@ -7,85 +7,92 @@
   </div>
 </template>
 <script>
-import { Group,Cell} from 'vux'
-export default {
-  mounted(){
-    this.setTitle('文章详情');
-    this.init();
-  },
-  data () {
-    return {
-      id:this.$route.query.id,
-      articleData:{},
-      isHtml:false,
-    }
-  },
-  methods:{
-    init(){
-      let self = this
-      self.$http.get('h9/api/article/'+self.id)
-        .then(function(res) {
-          if(res.data.code==0){
-              self.articleData=res.data.data
-            console.log(res.data.data)
-              if(self.articleData.content.indexOf('div')!=-1 || self.articleData.content.indexOf('p')!=-1){
-                self.isHtml=true
-              }else{
-                self.isHtml=false
+  import {Group, Cell} from 'vux'
+
+  export default {
+    mounted() {
+      this.setTitle('文章详情');
+      this.init();
+    },
+    data() {
+      return {
+        id: this.$route.query.id,
+        articleType: this.$route.query.type,
+        articleData: {},
+        isHtml: false,
+      }
+    },
+    methods: {
+      init() {
+        let self = this
+        if (this.articleType === 'hotel') {
+          this.$http.get('h9/api/hotel/info?hotelId=' + self.id).then(res => {
+            console.log(res.data)
+          })
+        } else {
+          self.$http.get('h9/api/article/' + self.id)
+            .then(function (res) {
+              if (res.data.code == 0) {
+                self.articleData = res.data.data
+                if (self.articleData.content.indexOf('div') != -1 || self.articleData.content.indexOf('p') != -1) {
+                  self.isHtml = true
+                } else {
+                  self.isHtml = false
+                }
               }
-          }
-        })
-    }
-  },
-   components: {
-    Group,Cell
-  },
-}
+            })
+        }
+      }
+    },
+    components: {
+      Group, Cell
+    },
+  }
 
 </script>
 <style scoped lang='less'>
-  .articlePage{
+  .articlePage {
     height: 100%;
     width: 97%;
     overflow-y: auto;
     background: #fff;
     padding-top: 3%;
     padding-left: 3%;
-    .title{
+    .title {
       font-size: 36/40rem;
       line-height: 58/40rem;
       color: #333;
     }
-    .date{
+    .date {
       color: #999;
       font-size: 32/40rem;
       margin: 30/40rem 0 60/40rem;
-      span{
+      span {
         margin-right: 10/40rem;
       }
     }
-    .content{
+    .content {
       color: #666;
-      font-size: 28/40rem!important;
+      font-size: 28/40rem !important;
       line-height: 46/40rem;
-      img{
+      img {
         width: 100%;
       }
     }
   }
 </style>
 <style lang='less'>
-  .articlePage{
-    .content{
+  .articlePage {
+    .content {
       color: #666;
-      font-size: 28/40rem!important;
-      font-size: 14px!important;
+      font-size: 28/40rem !important;
+      font-size: 14px !important;
       line-height: 46/40rem;
-      p{
-        font-size: 14px!important;
+      p {
+        font-size: 14px !important;
       }
-      .gameExplain-cont{
-        img{
+      .gameExplain-cont {
+        img {
           width: 100%;
         }
       }
