8112a275f9a176247362967c2cecd0e0c7b7c5e4
wudong <wudong@hey900.com>
2018-01-18 09:59:21 +0800

    测试接口

--- src/main.js
+++ src/main.js
@@ -196,7 +196,7 @@ let userdata = JSON.parse(localStorage.getItem('_user'))
 let url = document.location.href.split('#')[0]
 let seturl = ''
 
-if (url.indexOf('weixin-test-h9.thy360.com')!=-1 || url.indexOf('localhost')!=-1) {
+if (url.indexOf('weixin-testnew-h9.thy360.com')!=-1 || url.indexOf('localhost')!=-1) {
   seturl = 'https://weixin-testnew-h9.thy360.com'
 }else if (url.indexOf('weixin-dev-h9.thy360.com')!=-1){
   seturl = 'https://weixin-dev-h9.thy360.com'
