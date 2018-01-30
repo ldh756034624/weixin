a4c0f2195b6f216453a1be92accc0f92bf22a3de
DESKTOP-4G3U2U4\qiuzerong <qiuzerong@hey900.com>
2018-01-30 16:51:03 +0800

    添加酒店下拉加载

--- src/views/hotel/list.vue
+++ src/views/hotel/list.vue
@@ -28,17 +28,8 @@
     </transition>
     <!--酒店搜索出来的列表-->
     <div class="list-wrapper">
-      <scroller lock-x ref="scroll" 
-                :pullup-config="pulldefaultConfig"
-                use-pullup
-                :pullup-status="pullupStatus"
-                @on-pullup-loading="loadMore"
-                use-pulldown
-                @on-pulldown-loading="refresh"
-                :pulldown-config="pulldefaultConfig"
-                :pulldown-status="pulldownStatus"
-                @input="getCurrentValue"
-                height="-46">
+      <scroller lock-x height="-45" ref="scroller" @on-scroll-bottom="loadMore"
+              :scroll-bottom-offst="150">
         <div>
           <ul>
             <li class="hotel-item" v-for="item in hotelList" @click="handleHotel(item)">
@@ -60,16 +51,14 @@
               </div>
             </li>
           </ul>
+          <load-more tip="加载更多" v-if="showLoading"></load-more>
         </div>
-        <pull-header-footer v-if="showStatusFooter" :status-up="pullupStatus"
-                            :status-down="pulldownStatus"></pull-header-footer>
       </scroller>
     </div>
   </div>
 </template>
 <script>
-  import {Scroller} from 'vux'
-  import PullHeaderFooter from '@/components/pullHeaderFooter'
+  import {Scroller, LoadMore} from 'vux'
   export default {
     data() {
       return {
@@ -81,72 +70,60 @@
           page:1,
           limit:10
         },
+        hasMore: false, // 是否有更多
         showCity: false,  // 显示城市列表
-        showStatusFooter: false  // 底部没有更多等数据状态栏
+        showLoading: false, // 显示加载更多提示
       }
     },
     created() {
       this.getCityList()
-      this.init(1)
+      this.getList()
     },
     mounted() {
-      this.$nextTick(() => {
-        this.$refs.scroll.reset()
-      })
+      
     },
     methods: {
       /**
        * 获取酒店列表
        * @param page [页码]
        */
-      init(page) {
-        let self = this
-        if (page === 1) {
-          self.hotelList = []
-        }
-        this.query.page = page
+      getList(more) {
         let params = this.query
-        console.log(params)
-        self.$http.get('/h9/api/hotels',{params})
-          .then(function (res) {
-            if (res.data.code == 0) {
-              if (res.data.data.data.length > 0) {
-                self.hotelList = [...self.hotelList, ...res.data.data.data]
-                self.hasItem = true
-              } else {
-                self.hasItem = false
-              }
-              self.page.totalpage = res.data.data.totalPage;
-              if (res.data.data.hasNext) {
-                self.page.currPage++;
-              }
-              self.page.hasNext = res.data.data.hasNext;
+        this.$http.get('/h9/api/hotels', {params}).then(res => {
+          let data = res.data
+          if (data.code === 0) {
+            this.hasMore = data.data.hasNext
+            if (more) {
+              this.hotelList = [...this.hotelList, ...data.data.data]
+            } else {
+              this.hotelList = data.data.data
             }
-            self.$nextTick(() => {
-              self.$refs.scroll.donePullup();
-              if (!self.page.hasNext) {
-                self.$refs.scroll.disablePullup();
-              }
-              if (res.data.data.currPage == 1) {
-                self.$refs.scroll.reset({top: 0}, 500, 'ease');
-              }
-            });
+          }
+          this.$nextTick(() => {  // 请求数据后刷新高度
+            if(data.data.currPage === 1){
+              this.$refs.scroller.reset({top:0},500, 'ease')
+            }else{
+              this.$refs.scroller.reset()
+            }
           })
+        })
       },
-      loadMore() {
-        let self = this;
-        self.showStatusFooter = true
-        if (self.page.hasNext) {
-          setTimeout(() => {
-            self.init(self.page.currPage)
-          }, 2000)
+      loadMore() {  // 上拉加载更多
+        if (!this.hasMore) {
+          return
         }
-      },
-      refresh() {
-        let self = this;
-        setTimeout(() => {
-          self.init(1);
-        }, 2000)
+        if (this.onFetching) { // 正在加载
+          return
+        } else {
+          this.onFetching = true
+          this.showLoading = true
+          setTimeout(() => {
+            ++this.query.page
+            this.getList(true)
+            this.onFetching = false
+            this.showLoading = false
+          }, 1500)
+        }
       },
       // 获取城市列表
       getCityList() {
@@ -167,9 +144,10 @@
       },
       // 选择城市区域
       handleChooseCity(item) {
+        this.query.page = 1
         this.query.city = item
+        this.getList()
         this.showCity = false
-        this.init(1)
       },
       // 选择酒店
       handleHotel(item) {
@@ -182,15 +160,16 @@
         clearTimeout(this.timer)
         this.timer = setTimeout(() => {
           // 发起请求
-          this.init()
+          this.getList()
         }, 1000)
       },
     },
     components: {
       Scroller,
-      PullHeaderFooter
+      LoadMore
     },
   }
+
 </script>
 
 <style scoped lang='less'>
