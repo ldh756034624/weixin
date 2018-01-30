bdf22387204affc8c56a0c6ad420154d917c07f4
DESKTOP-4G3U2U4\qiuzerong <qiuzerong@hey900.com>
2018-01-30 18:06:33 +0800

    路由跳转

--- src/main.js
+++ src/main.js
@@ -59,14 +59,14 @@ router.beforeEach((to, from, next) => {
   } else if (from.path === '/hotel/success' && sessionStorage.paySuccess == 'true') { // 如果从酒店支付成功页来到首页
     sessionStorage.paySuccess = 'false'
     next('/index')
-  } else if(from.path === '/hotel/fail' && sessionStorage.payCheckOrder == 'true' ){  //从酒店支付失败页来到首页
-    sessionStorage.payCheckOrder = 'false'
+  } else if(from.path === '/hotel/fail' && sessionStorage.paySuccess == 'true' ){  //从酒店支付失败页来到首页
+    sessionStorage.paySuccess = 'false'
     next('/index')
   } else if(from.path === '/hotel/orderDetail' && sessionStorage.orderListPaySuccess == "true"){
     sessionStorage.orderListPaySuccess = 'false'
     next('/hotel/orderList')
-  } else if(from.path === '/hotel/orderDetail' && sessionStorage.orderListPaySuccess == "true"){
-    sessionStorage.orderListPaySuccess = 'false'
+  } else if(from.path === '/hotel/orderDetail' && sessionStorage.paySuccess == "true"){
+    sessionStorage.paySuccess = 'false'
     next('/index')
   } else {
     next()
