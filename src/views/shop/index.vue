3ac38d058d978bfc367b6ca8683b9fcb2b118f83
ken.hong <ken.hong@hey900.com>
2018-02-12 10:50:36 +0800

    swiper调整

--- src/views/shop/index.vue
+++ src/views/shop/index.vue
@@ -4,8 +4,8 @@
       <scroller lock-x scrollbar-y ref="detailScroller" use-pulldown height="100%" @on-pulldown-loading="refresh"
                 v-model="status1">
         <div class="mrgb" >
-          <swiper dots-position="center" height="9.375rem" auto dots-class="custom-bottom">
-            <swiper-item class="swiper-demo-img" v-for="(item, index) in shopTop" height="12.5rem" :key="index"
+          <swiper dots-position="center" height="9.375" :aspect-ratio="750/375" auto dots-class="custom-bottom">
+            <swiper-item class="swiper-demo-img" v-for="(item, index) in shopTop"  :key="index"
                          @click.native='goLinkFn(item)'>
               <img :src="item.imgUrl" width="100%" height="100%">
             </swiper-item>
@@ -282,7 +282,7 @@
   .pulldown-arrow {
     font-size: 24/40rem;
   }
-  #shopBanner .vux-swiper{
+  /*#shopBanner .vux-swiper{
     height: 375/40rem!important;
-  }
+  }*/
 </style>
